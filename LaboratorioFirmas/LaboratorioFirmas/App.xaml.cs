84bf0d05632005b35b84ff564b1a5695dc3e2f60
AcosMar <acostaalejandro68@gmail.com>
2021-11-23 22:54:10 -0600

    Firmas Digitales

---
+++ LaboratorioFirmas/LaboratorioFirmas/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LaboratorioFirmas
+{
+    public partial class App : Application
+    {
+        internal static object SQLiteDB;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
